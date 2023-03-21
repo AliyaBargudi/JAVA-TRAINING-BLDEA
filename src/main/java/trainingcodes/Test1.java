0f7c1f9597ba380089e84c7d77a891b2612600f1
Soheb <Soheb@DESKTOP-348BACP>
2023-03-21 15:14:01 +0530

    Second commit

---
+++ src/main/java/trainingcodes/Test1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package trainingcodes;
+
+/**
+ * @author Soheb
+ *
+ */
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+	}
+
+}
